b24db49566d0aac9bc4824f3c90441e29ae97fc9
arthur revelski <arevelski@managedmethods.com>
2024-08-15 22:04:02 -0600

    file cleanup, updating bios, deleting unused files

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Arthur Revelski - Front End Focused Software Engineer';
+export const SITE_DESCRIPTION = 'Welcome to my blog! I’m Arthur Revelski, a Front End Software Engineer who creates intuitive user interfaces with robust functionality. Here, I share insights on software development, new recipes I try, and my adventures exploring the Colorado mountains.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
