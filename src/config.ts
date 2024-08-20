51fc736ca2fb2fffe492d729af12614c62428702
arthur revelski <arevelski@managedmethods.com>
2024-08-19 23:39:48 -0600

    intitial theme selector setup

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,4 @@
 export const SITE_TITLE = 'Arthur Revelski - Front End Focused Software Engineer';
 export const SITE_DESCRIPTION = 'Welcome to my blog! I’m Arthur Revelski, a Front End Software Engineer who creates intuitive user interfaces with robust functionality. Here, I share insights on software development, new recipes I try, and my adventures exploring the Colorado mountains.';
 export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
+export const TRANSITION_API = false
